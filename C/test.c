cbbbda2bf5f46cda744e3c8502983ca57fc5edf9
vsha96 <ivan.shulyugin@yandex.ru>
2019-09-30 17:41:45 +0300

    making shell

--- C/test.c
+++ C/test.c
@@ -104,46 +104,90 @@ int StringSize(const struct string *str)
 }
 
 /*we need some procedures to separate string into words*/
-/*
-struct string *StringMakeNewFrom(const struct string *str,int s1,int s2)
+
+struct string *StringMakeNewFrom(struct string *str,int s1,int s2)
 {
-	const struct string *temp;
-	struct string *newstr;
-	int i=0,j=0;
+	struct string *temp, *newstr, *newtemp;
+	int i,j,l,m;
+	temp = str;
+	s2 = s2 - s1; //pass few lines we'll make another base
+//printf("\n%i\n",s2);
 	//move to our 1st separator
-	for (j=0;j<=s1;j++)
+	for (i=0;i<(s1/chunk_size);i++)
 	{
-		return 0;
+		temp = temp->next;
+	}
+	s1 = s1 % chunk_size; //we made another base
+//printf("\n%i\n",s1);
+	s2 = s1 + s2; //s2 points in new base
+//printf("\n%i\n",s2);
+	newstr = malloc(sizeof(*newstr));
+	newtemp = newstr;
+	//copy every chunk we needed
+	for (i=0;i<=(s2/chunk_size);i++)
+	{
+		//if first chunk
+		if (i==0)
+		{
+			if (s2/chunk_size!=0) //if 2nd separator not in our chunk
+			{
+				for (j=s1-1;j<chunk_size;j++)
+				{
+					newtemp->x[j-s1+1]=temp->x[j];
+				}
+			} else { // 2nd separator in our chunk, we need ending
+				for (j=s1-1;j<s2;j++)
+				{
+					newtemp->x[j-s1+1]=temp->x[j];
+				}
+				//ending
+				if (j != chunk_size)
+				{
+					newtemp->x[j-s1+1]=EOF;
+				}
+				newtemp->next=NULL;
+			}
+		}
+		//if last chunk
+		
+		//else
 	}
 	
+	//newtemp->next=NULL;
 	
 	return newstr;
 }
-*/
 
-void StringSeparate(const struct string *str)
+
+void StringSeparate(struct string *str)
 {
 	
 }
 
 int main()
 {
-	struct string *str;
+	struct string *str,*str1;
 	printf("Input string:\n");
 	str = StringFill();
 	
 	printf("Your string:\n");
 	StringPrint(str);
 	//printf("\n");
-	
-	/*
 	printf("Size of your string:\n");
 	printf("%i\n",StringSize(str));
-	*/
+	/*
 	StringFree(str);
 	str = NULL;
 	printf("Your string after delete:\n");
 	StringPrint(str);
+	*/
+	str1 = StringMakeNewFrom(str,1,8);
+	printf("Ive made another string\n");
+	StringPrint(str1);
+	printf("\n");
+	
+	printf("Which size is\n%i\n",StringSize(str1));
+	
 	
 	return 0;
 }
