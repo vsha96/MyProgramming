8512d7abc04c813e16d4bd666a47e66b053b557b
vsha96 <ivan.shulyugin@yandex.ru>
2019-12-08 15:02:55 +0300

    more training

--- C/server/server.c
+++ C/server/server.c
@@ -9,32 +9,34 @@
 #define BUFSIZE 128
 #define MAX_CLIENT_FDS 128
 
-void error_handler()
+int error_handler()
 {
 	perror(NULL);
 	fflush(stderr);
+	return 1;
 }
 
-int main()
+int ServerSetup(int port, int *ls)
 {
-	int ls;
 	struct sockaddr_in addr;
-	ls = socket(AF_INET, SOCK_STREAM, 0);
-	if (ls == -1)
-	{
-		error_handler(); return 1;
-	}
+	*ls = socket(AF_INET, SOCK_STREAM, 0);
+	if (*ls == -1)
+		return error_handler();
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = INADDR_ANY;
-	addr.sin_port = htons(7777);
-	if ( -1 == bind(ls, (struct sockaddr *)&addr, sizeof(addr)))
-	{
-		error_handler(); return 1;
-	}
-	if (-1 == listen(ls, 16))
-	{
-		error_handler(); return 1;
-	}
+	addr.sin_port = htons(port);
+	if (-1 == bind(*ls, (struct sockaddr *)&addr, sizeof(addr)) ||
+		-1 == listen(*ls, 16))
+		return error_handler();
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int ls;
+	int port = 14500;
+	if (!ServerSetup(port, &ls))
+
 	for(;;)
 	{
 		int fd, fds[MAX_CLIENT_FDS];
