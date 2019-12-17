22786b29b7ed8b5a646b2c6d0fc7b5a6fa67d2eb
vsha96 <ivan.shulyugin@yandex.ru>
2019-12-17 10:00:36 +0300

    server with game (bank,market,players) (try it via telnet)

--- C/errors/enumError.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-   enum colors {red,green,blue}; /*delete last symbol*/
-   //is this a comment?
-   printf("hello, I've just declared enum type\n");
-   
-   
-}
--- C/errors/stringWithIdError.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-   char *p = "abcdef";
-   printf("%s \n",p);
-   p[0]='A';
-   printf("%s \n",p);
-   
-}
