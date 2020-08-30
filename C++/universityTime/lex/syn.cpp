87f211e17ba85f3fabc7b1d50303194c8226aeb6
vsha96 <ivan.shulyugin@yandex.ru>
2020-08-30 10:30:58 +0300

    grammar and syntax

--- C++/universityTime/lex/syn.cpp
+++ C++/universityTime/lex/syn.cpp
@@ -1,2 +1,44 @@
 #include "lex.hpp"
 #include "syn.hpp"
+
+ErrLex::ErrLex(List *lex, int code, const char* msg)
+{
+	err_lex = lex;
+	err_code = code;
+	err_msg = msg;
+}
+
+ErrLex::ErrLex(const ErrLex &other)
+{
+	//TODO	
+}
+
+ErrLex::~ErrLex()
+{
+	//TODO
+}
+
+Syn::Syn()
+{
+	cl = NULL;
+}
+
+void Syn::CheckGrammar(List *first)
+{
+	cl = first;
+}
+
+bool Syn::go()
+{
+	if (cl) {
+		cl = cl->next;
+	} else {
+		printf("WARN: Syn::go: &current_lex = NULL\n");
+		return false;
+	}
+	return true;
+}
+
+
+
+
