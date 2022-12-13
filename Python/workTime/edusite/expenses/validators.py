c3c114ee1a4fdc19a53f90f9e3afde3553ba37b5
vsha96 <vsha96@yandex.ru>
2022-12-13 14:02:02 +0700

    django + REST API

--- Python/workTime/edusite/expenses/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_money(value):
-    if value <= 0:
-        raise ValidationError(
-            ('Ticket money must be > 0'),
-            params={'value': value},)
